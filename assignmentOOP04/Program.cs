ca31ddc033a4018a5934656252e818cc8247b662
nancykhalid88 <42310142.Nancy@acu.edu.eg>
2026-03-03 22:15:10 +0200

    Q1

--- assignmentOOP04/Program.cs
+++ assignmentOOP04/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace assignmentOOP04
 {
+
+    #region Q1
+    //Q1 : What is the difference between static binding and dynamic binding? 
+    //    When does each one happen?
+    //method overloading and non virtual methods use static binding which happens at compile time
+    //while overriding and virtual methods use dnamic binding which happens at runtime 
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
 }
