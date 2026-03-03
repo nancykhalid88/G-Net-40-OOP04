b342f8a8c0ea47981f94c88e7e1190bef8603896
nancykhalid88 <42310142.Nancy@acu.edu.eg>
2026-03-03 22:24:45 +0200

    Q2

--- assignmentOOP04/Program.cs
+++ assignmentOOP04/Program.cs
@@ -7,6 +7,11 @@
     //method overloading and non virtual methods use static binding which happens at compile time
     //while overriding and virtual methods use dnamic binding which happens at runtime 
     #endregion
+    #region Q2
+    //Q2 :  What is the difference between method overloading and method overriding?
+    //overloading means writing the same function name but the change happens in the parameters and it happens at compile time (static binding)
+    //while overriding is changing the method implementation using virtual or override and happens at runtime (dynamic binding) 
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
